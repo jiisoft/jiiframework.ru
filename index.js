e0195ca75fc195d618a5250ceda3fd2c677779cd
Kozhin Vladimir <affka@affka.ru>
2015-08-16 09:46:00 +0700

    run server via jii-workers

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-require('./bootstrap');
+global.Jii = require('jii');
+require('jii-httpserver');
+require('jii-model');
+require('jii-view');
+require('jii-assets');
 
-Jii.app.http.start();
+global.app = Jii.namespace('app');
+require('require-all')(__dirname + '/assets');
+require('require-all')(__dirname + '/controllers');
+require('require-all')(__dirname + '/models');
+
+var custom = require('./config');
+
+require('jii-workers')
+    .setEnvironment(custom.environment)
+    .application(Jii.mergeConfigs(
+        // Main config
+        {
+            workers: 2,
+            application: {
+                basePath: __dirname,
+                components: {
+                    urlManager: {
+                        className: 'Jii.urlManager.UrlManager',
+                        rules: {
+                            '': 'site/index',
+                            'guide': 'site/guide',
+                            'guide/<page>': 'site/guide',
+                            'contact': 'site/contact'
+                        }
+                    },
+                    http: {
+                        className: 'Jii.httpServer.HttpServer',
+                        staticDirs: __dirname + '/web/'
+                    },
+                    view: {
+                        className: 'Jii.view.ServerWebView'
+                    },
+                    assetManager: {
+                        className: 'Jii.assets.AssetManager',
+                        bundles: {
+                            'app.assets.AppAsset': {},
+                            'app.assets.BootstrapAsset': {},
+                            'app.assets.JqueryAsset': {}
+                        },
+                        baseUrl: '/assets'
+                    }
+                }
+            }
+        },
+
+        // Custom configuration
+        custom.main
+    ));
